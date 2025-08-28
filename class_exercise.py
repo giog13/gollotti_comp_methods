0f5d05d47c9cabe14e1ca8216022c1ef517876e2
Giovanni Gollotti <astrog13@uw.edu>
2025-08-28 11:30:17 -0400

    Created notebook and .py files for class exercises

---
+++ class_exercise.py
@@ -0,0 +1,9 @@
+import numpy as np
+import sys
+
+def drop_time(h, g = 9.8): #h = positional, g = keyword
+    t = ((2.0 * h) / g) ** (1/2) #Avoid using a package unless absolutely necessary
+    return t
+
+if __name__ == "__main__": #Runs from command line
+    print(drop_time(np.float(sys.argv[1]))) #index 0 = .py filename
